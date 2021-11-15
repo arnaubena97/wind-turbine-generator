08ca9363f842ea187d4b541f5750ade2b348250f
arnaubena <arnaubenavides97@gmail.com>
2021-11-15 17:01:15 +0100

    MQTT Done

--- Arduino/MQTTBrokerServer/MQTTBrokerServer.ino
+++ Arduino/MQTTBrokerServer/MQTTBrokerServer.ino
@@ -1,80 +1,68 @@
 #include <Wire.h>
 #include <Adafruit_MCP23X17.h>
+#include <ESP8266WiFi.h>
+#include "uMQTTBroker.h"
+
 
 //Global variables
 Adafruit_MCP23X17 mcp;
-int serial = 0;
+uMQTTBroker myBroker;
 
-void setup() {  
+/*
+ * Your WiFi config here
+ */
+char ssid[] = "ssid";      // your network SSID (name)
+char pass[] = "password"; // your network password
+
+const byte numPins = 4;
+byte pins[] = {0, 1, 2, 3};
+byte var;
+void setup() { 
   Serial.begin(9600);
+  WiFi.softAP(ssid, pass);
+  Serial.println("AP started");
+  Serial.println("IP address: " + WiFi.softAPIP().toString());
+  
+  Serial.println("Starting MQTT broker");
+  myBroker.init();
+  myBroker.subscribe("#");
+
+  
+  
   // Initialize the MCP23017
   if (!mcp.begin_I2C()) { // Try to connect to MCP
     Serial.println("Error.");
     while (1);
   }
-  //Pins configurations
-  mcp.pinMode(0, OUTPUT);
-  mcp.pinMode(1, OUTPUT);
-  mcp.pinMode(2, OUTPUT);
-  mcp.pinMode(3, OUTPUT);
-  mcp.pinMode(4, OUTPUT);
+  for (int i=0; i<numPins; i++) {
+    mcp.pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
+  
   // variabe will change every loop
-  if(serial==0){
-    mcp.digitalWrite(0, LOW);
-    mcp.digitalWrite(1, LOW);
-    mcp.digitalWrite(2, LOW);
-    mcp.digitalWrite(3, LOW);
-    mcp.digitalWrite(4, LOW);
+  var = myBroker.getClientCount();
+  //uint16_t vnum = myBroker.getClientCount();
+  //Serial.print("Connections: ");
+  //Serial.println(var);
+  int varint= (int) var;
+  Serial.print("Connections: ");
+  Serial.println(varint);
+  for (int q=0; q<numPins; q++) {
+    if(q<varint){
+      //Serial.print("PIN HIGH : ");
+      //Serial.println(pins[q]);
+      mcp.digitalWrite(pins[q], HIGH);
+      
+    }else{
+      //Serial.print("PIN LOW : ");
+      //Serial.println(pins[q]);
+       mcp.digitalWrite(pins[q], LOW);
     }
-    else if(serial==1){
-    mcp.digitalWrite(0, HIGH);
-    mcp.digitalWrite(1, LOW);
-    mcp.digitalWrite(2, LOW);
-    mcp.digitalWrite(3, LOW);
-    mcp.digitalWrite(4, LOW);
-    }
-    else if(serial==2){
-    mcp.digitalWrite(0, HIGH);
-    mcp.digitalWrite(1, HIGH);
-    mcp.digitalWrite(2, LOW);
-    mcp.digitalWrite(3, LOW);
-    mcp.digitalWrite(4, LOW);
-    }
-    else if(serial==3){
-    mcp.digitalWrite(0, HIGH);
-    mcp.digitalWrite(1, HIGH);
-    mcp.digitalWrite(2, HIGH);
-    mcp.digitalWrite(3, LOW);
-    mcp.digitalWrite(4, LOW);
-    }
-    else if(serial==4){
-    mcp.digitalWrite(0, HIGH);
-    mcp.digitalWrite(1, HIGH);
-    mcp.digitalWrite(2, HIGH);
-    mcp.digitalWrite(3, HIGH);
-    mcp.digitalWrite(4, LOW);
-    }
-    else if(serial==5){
-    mcp.digitalWrite(0, HIGH);
-    mcp.digitalWrite(1, HIGH);
-    mcp.digitalWrite(2, HIGH);
-    mcp.digitalWrite(3, HIGH);
-    mcp.digitalWrite(4, HIGH);
-    }
+    //delay(1200);
+  }    
+ 
   delay(500);
 
-  
-  /* BLINK EXAMPLE
-  // Switch LED on
-  mcp.digitalWrite(0, HIGH);
-  
-  delay(500);
-  
-  // Switch LED off
-  mcp.digitalWrite(0, LOW);
-  
-  */
 }
