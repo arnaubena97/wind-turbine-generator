031e2af3624b1c5d47442e8ee7b68e2448e5d711
arnaubena <arnaubenavides97@gmail.com>
2021-12-09 16:36:35 +0100

    bug solved

--- Arduino/DataProducer2/DataProducer2.ino
+++ Arduino/DataProducer2/DataProducer2.ino
@@ -64,10 +64,11 @@ void setup_wifi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  //quit comment if not work
+  //while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-  }
+  //}
 
   randomSeed(micros());
 
